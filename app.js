59db3a58386f6ab245d31716a1c339c1378be5cb
pankajkumar155 <87750349+pankajkumar155@users.noreply.github.com>
2021-08-29 16:10:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-  var div = this.parentElement;
-  div.style.display = "none";
-  }
-}
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-  ev.target.classList.toggle('checked');
-  }
-}, false);
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-  alert("You must write something!");
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer1 button");
+// onkeyup event
+inputBox.onkeyup = () => {
+  let userEnteredValue = inputBox.value; //getting user entered value
+  if (userEnteredValue.trim() != 0) {
+    
+    addBtn.classList.add("active"); //active the add button
   } else {
-    document.getElementById("myUL").appendChild(li);
+    addBtn.classList.remove("active"); //unactive the add button
   }
-  document.getElementById("myInput").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
+};
+showTasks(); 
+addBtn.onclick = () => {
+  
+  let userEnteredValue = inputBox.value; 
+  let getLocalStorageData = localStorage.getItem("New Todo"); 
+  if (getLocalStorageData == null) {
+    
+    listArray = []; //create a blank array
+  } else {
+    listArray = JSON.parse(getLocalStorageData); //transforming json string into a js object
   }
+  listArray.push(userEnteredValue); //pushing or adding new value in array
+  localStorage.setItem("New Todo", JSON.stringify(listArray)); //transforming js object into a json string
+  showTasks(); 
+  addBtn.classList.remove("active"); //unactive the add button once the task added
+};
+function showTasks() {
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  if (getLocalStorageData == null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(getLocalStorageData);
+  }
+  const pendingTasksNumb = document.querySelector(".pendingTasks");
+  pendingTasksNumb.textContent = listArray.length; //passing the array length in pendingtask
+  if (listArray.length > 0) {
+    //if array length is greater than 0
+    deleteAllBtn.classList.add("active"); 
+  } else {
+    deleteAllBtn.classList.remove("active"); 
+  }
+  let newLiTag = "";
+  listArray.forEach((element, index) => {
+    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+  });
+  todoList.innerHTML = newLiTag; 
+  inputBox.value = ""; 
 }
-function removeAll(){
-  var lst = document.getElementsByTagName("ul");
-    lst[0].innerHTML = "";
+
+function deleteTask(index) {
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  listArray = JSON.parse(getLocalStorageData);
+  listArray.splice(index, 1); 
+  localStorage.setItem("New Todo", JSON.stringify(listArray));
+  showTasks(); 
 }
+
+deleteAllBtn.onclick = () => {
+  listArray = []; //empty the array
+  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
+  showTasks(); 
+};
