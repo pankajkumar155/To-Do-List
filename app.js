0c30b31cdc39f5663e31000fd63f14f0844e0142
pankajkumar155 <87750349+pankajkumar155@users.noreply.github.com>
2021-08-29 20:42:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-function addMore() {
-    document.getElementById("error").innerHTML = "";
-    let name = document.getElementById("name").value;
-    if (name == '') {
-        document.getElementById("error").innerHTML = "Please, enter something.";
+  
+$('form').on('click', 'input[type=submit]', function(e) {
+    e.preventDefault();
+    let task = $('input[type=text]').val();
 
-    } else {
-        let box = document.getElementById("box");
-        let li = document.createElement("li");
-        li.innerHTML = name;
-        let a = document.createElement("a");
-        a.innerHTML = "X";
-        a.href = "javascript:void(0)";
-        a.className = "remove";
-        li.appendChild(a);
-        let pos = box.firstElementChild;
-        if (pos == null) {
-            box.appendChild(li);
+    if (task.length > 0) {
+        $('ul.task-list').append('<li><input type=checkbox><p>' + task + '</p><img src="close.png" alt="close"></li>');
+        $('input[type=text]').val('');
+    };
+
+    $('input[type=checkbox]').click(function(){
+        if ($(this).is(':checked')) {
+            $(this).parent('li').fadeTo(300, 0.4)
         } else {
-            box.insertBefore(li, pos);
+            $(this).parent('li').fadeTo(300, 1);
         }
-
-
-    }
-    document.getElementById("name").value = "";
-}
-let btn = document.getElementById("box");
-btn.addEventListener('click', function (e) {
-
-    let litt = e.target.parentNode;
-    box.removeChild(litt);
-
-}
-)
-
-let btn1 = document.getElementById("btnClick1");
-btn1.addEventListener('click', function () {
-    document.getElementById("box").innerHTML = "";
+    });
+  
+    $(function (){
+        $('img').click(function(){
+            $(this).parent('li').fadeOut();
+        })
+        
+    });
 })
